3bb54d330f1bfdd227eed494f4cdad7dd30961c7
Hamza Shahzad <hamzashahzad604@gmail.com>
2021-04-11 15:51:42 +0500

    Firebase Storage Integrated.

--- app/src/main/java/com/example/lecture1batch3java/CreateAccountActivity.java
+++ app/src/main/java/com/example/lecture1batch3java/CreateAccountActivity.java
@@ -1,9 +1,12 @@
 package com.example.lecture1batch3java;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -14,19 +17,35 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
+    private static final int RESULT_LOAD_IMG = 1000;
     private EditText text_name, text_email, text_password;
     private Button btn_sign_up;
     private ImageView userImage;
     private FirebaseAuth mAuth;
+    private FirebaseStorage firebaseStorage;
+    private  Uri imageUri;
+    private StorageReference storageRef;
+
+    // Create a Cloud Storage reference from the app
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,25 +56,63 @@ public class CreateAccountActivity extends AppCompatActivity {
         text_email = findViewById(R.id.text_email);
         text_password = findViewById(R.id.text_password);
         userImage = findViewById(R.id.userImage);
+        btn_sign_up = findViewById(R.id.btn_sign_up);
 
         userImage.setImageResource(R.drawable.ic_launcher_background);
+        userImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG);
+            }
+        });
 
         Picasso
                 .get()
-                .load("https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80")
+                .load("https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1214428300?k=6&m=1214428300&s=170667a&w=0&h=hMQs-822xLWFz66z3Xfd8vPog333rNFHU6Q_kc9Sues=")
                 .placeholder(R.drawable.ic_launcher_background)
                 .fit()
                 .centerInside()
                 .into(userImage);
 
-        btn_sign_up = findViewById(R.id.btn_sign_up);
+
 
         mAuth = FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageRef = firebaseStorage.getReference();
+
 
         View.OnClickListener btnSignUpListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("TAG", "onClick: btnSignUpListener");
+
+                /* UploadTask uploadTask = storageRef.child(imageUri.getLastPathSegment()).putFile(imageUri);
+                uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if (!task.isSuccessful()) {
+                            throw task.getException();
+                        }
+
+                        // Continue with the task to get the download URL
+                        return storageRef.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()) {
+                            imageUri = task.getResult();
+                            Log.i("TAG", "onComplete: " + imageUri);
+
+                        } else {
+                            // Handle failures
+                            // ...
+                        }
+                    }
+                }); */
+
                 if (text_name.getText().toString().equals("")){
                    text_name.setError("Field is required");
                 } else if (text_email.getText().toString().equals("")) {
@@ -76,9 +133,82 @@ public class CreateAccountActivity extends AppCompatActivity {
                                         Log.d("TAG", "createUserWithEmail:success");
                                         FirebaseUser user = mAuth.getCurrentUser();
                                         if (user != null) {
-                                            startActivity(new Intent(CreateAccountActivity.this, TimelineActivity.class));
-                                            finish();
+
+                                            UserProfileChangeRequest.Builder userBuilder = new UserProfileChangeRequest.Builder();
+                                            userBuilder.setDisplayName(text_name.getText().toString());
+                                            // userBuilder.setPhotoUri(imageUri);
+
+                                            UserProfileChangeRequest userProfileChangeRequest = userBuilder.build();
+
+                                            user.updateProfile(userProfileChangeRequest).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    startActivity(new Intent(CreateAccountActivity.this, TimelineActivity.class));
+                                                    finish();
+                                                }
+                                            });
+                                            StorageReference imageReference = storageRef.child("profile_images/"+imageUri.getLastPathSegment());
+                                            UploadTask uploadTask = imageReference.putFile(imageUri);
+                                            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                                                @Override
+                                                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                                                    if (!task.isSuccessful()) {
+                                                        throw task.getException();
+                                                    }
+
+                                                    // Continue with the task to get the download URL
+                                                    return task.getResult().getStorage().getDownloadUrl();
+                                                }
+                                            }).addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                @Override
+                                                public void onSuccess(Uri uri) {
+                                                    Log.i("TAG", "onSuccess: photo-url = " + uri);
+
+                                                    imageUri = uri;
+
+                                                    UserProfileChangeRequest.Builder userBuilder = new UserProfileChangeRequest.Builder();
+                                                    userBuilder.setDisplayName(text_name.getText().toString());
+                                                    userBuilder.setPhotoUri(imageUri);
+
+                                                    UserProfileChangeRequest userProfileChangeRequest = userBuilder.build();
+
+                                                    user.updateProfile(userProfileChangeRequest).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            startActivity(new Intent(CreateAccountActivity.this, TimelineActivity.class));
+                                                            finish();
+                                                        }
+                                                    });
+                                                }
+                                            });/*.addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Uri> task) {
+                                                    if (task.isSuccessful()) {
+                                                        imageUri = task.getResult();
+
+                                                        UserProfileChangeRequest.Builder userBuilder = new UserProfileChangeRequest.Builder();
+                                                        userBuilder.setDisplayName(text_name.getText().toString());
+                                                        userBuilder.setPhotoUri(imageUri);
+
+                                                        UserProfileChangeRequest userProfileChangeRequest = userBuilder.build();
+
+                                                        user.updateProfile(userProfileChangeRequest).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                startActivity(new Intent(CreateAccountActivity.this, TimelineActivity.class));
+                                                                finish();
+                                                            }
+                                                        });
+
+                                                    } else {
+                                                        // Handle failures
+                                                        // ...
+                                                    }
+                                                }
+                                            });*/
+
                                         }
+
                                     } else {
                                         // If sign in fails, display a message to the user.
                                         Log.w("TAG", "createUserWithEmail:failure", task.getException());
@@ -97,4 +227,18 @@ public class CreateAccountActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+
+        if (reqCode == RESULT_LOAD_IMG && resultCode == RESULT_OK) {
+            imageUri = data.getData();
+            userImage.setImageURI(imageUri);
+            Toast.makeText(CreateAccountActivity.this, "You have picked Image",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(CreateAccountActivity.this, "You haven't picked Image",Toast.LENGTH_LONG).show();
+        }
+
+    }
 }
--- app/src/main/java/com/example/lecture1batch3java/TimelineActivity.java
+++ app/src/main/java/com/example/lecture1batch3java/TimelineActivity.java
@@ -7,8 +7,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
 
 public class TimelineActivity extends AppCompatActivity {
 
@@ -19,6 +22,8 @@ public class TimelineActivity extends AppCompatActivity {
     // SharedPreferences.Editor editorUserInfo;
 
     private FirebaseAuth mAuth;
+    private ImageView userImage;
+    private TextView userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +33,26 @@ public class TimelineActivity extends AppCompatActivity {
         // sharedPreferenceUserInfo = getSharedPreferences("user_info", MODE_PRIVATE); // Read
         // editorUserInfo = sharedPreferenceUserInfo.edit();  // Edit (Write, Replace, Delete/Remove)
 
+        userImage = findViewById(R.id.userImage);
+        userName = findViewById(R.id.userName);
 
         mAuth = FirebaseAuth.getInstance();
+
+        Log.i("TAG", "onCreate: photo-url = " + mAuth.getCurrentUser().getPhotoUrl());
+        Picasso
+                .get()
+                .load(mAuth.getCurrentUser().getPhotoUrl())
+                .error(R.drawable.ic_launcher_foreground)
+                .placeholder(R.drawable.ic_launcher_background)
+                .fit()
+                .centerInside()
+                .into(userImage);
+
+        userName.setText(mAuth.getCurrentUser().getDisplayName());
 
         Intent receivedIntent = getIntent();
         Log.i("TAG", "onCreate: " + "Email: " + receivedIntent.getStringExtra("login_email") + " isLoggedIn: " + receivedIntent.getBooleanExtra("isLoggedIn", true));
         // Log.i("TAG", "onCreate: SHARED_PREFERENCES " + "Email: " + sharedPreferenceUserInfo.getString("login_email", "") + " isLoggedIn: " + sharedPreferenceUserInfo.getBoolean("isLoggedIn", false));
-
 
     }
 
@@ -44,10 +62,3 @@ public class TimelineActivity extends AppCompatActivity {
         finish();
     }
 }
-
-
-/*
-SplashScreen >>
-        Main (Login / SignUp) >>
-            Login / SignUp Screens >>
-                Timeline*/
